7df6451cb75297be6ad18dc7bb50bec3c39edd4b
douglascodes <douglasking215@gmail.com>
2013-07-27 18:32:59 -0400

    Factored out common code to UnitOfLanguage trait with template

--- src/main/scala/word.scala
+++ src/main/scala/word.scala
@@ -1,45 +1,48 @@
 package com.DouglasCodes.Cryptogram_scala.Word
 
+object LetterPool {
+  private val alphabet: Set[Char] = ('A' to 'Z').toSet
+  private val pool: Map[Char, Letter] =
+  { for ( a <- alphabet)
+    yield (a, new Letter(a) )}.toMap
 
-class Letter(val name: Char, val initial: Set[Char] ) {
+  def refer(x: Char): Letter =
+    pool(x)
+}
+
+trait UnitOfLanguage[T] {
+    var possibles: Set[T]
+
+    def possibilities: Int =
+      possibles.size
+
+    def singular: Boolean =
+      possibilities == 1
+
+    def canBe(x: T): Boolean =
+      possibles.contains(x)
+
+    def reassign(p: Set[T] ) =
+      possibles = p
+
+}
+
+class Letter(val name: Char, val initial: Set[Char] ) extends UnitOfLanguage[Char] {
   def this(name: Char) =
     this(name: Char, ('A' to 'Z').toSet - name)
 
-  private var possibles: Set[Char] =
+  var possibles: Set[Char] =
     initial
-
-  def possibilities: Int =
-    possibles.size
-
-  def singular: Boolean =
-    possibilities == 1
-
-  def canBe(x: Char): Boolean =
-    possibles.contains(x)
-
-  def reassign(p: Set[Char] ) =
-    possibles = p
 
   override def toString: String =
     name + possibles.mkString(": { ", ", ", " }\n")
 }
 
-class Word(val name: String ) {
-  val value: Set[Letter] = name.toSet.map( (x: Char)=> LetterPool.refer(x) )
+class Word(val name: String ) extends UnitOfLanguage[String] {
+  val value = for (i: Char <- name.toSet)
+    yield LetterPool.refer(i)
 
-  private var possibles: Set[String] = Set(name)
-
-  def possibilities: Int =
-    possibles.size
-
-  def singular: Boolean =
-    possibilities == 1
-
-  def canBe(x: String): Boolean =
-    possibles.contains(x)
-
-  def reassign(p: Set[String] ) =
-    possibles = p
+  var possibles: Set[String] = Set(name)
 
   override def toString: String =
     name + "\n" +
@@ -47,9 +50,4 @@ class Word(val name: String ) {
           yield p
       }.mkString("","","") +
      possibles.mkString("{ ", " ", " }")
-}
-
-object LetterPool {
-   val pool: Hash[Char, Letter] =
-
 }
