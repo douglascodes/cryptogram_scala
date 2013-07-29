556f5f941f091dc9f274e3c8524e25073c3183c0
douglascodes <douglasking215@gmail.com>
2013-07-29 02:11:02 -0400

    Added update() feature to Word class. Added Unique length and pattern to dictionary map.

--- src/main/scala/uol.scala
+++ src/main/scala/uol.scala
@@ -21,3 +21,13 @@ val OrderedUniqueString: Function1[String, String] =
   (name: String) =>
     name.foldLeft(new String)((a, b) =>
       { if (a.contains(b)) a else a + b} )
+
+val NumericPatternOfString: Function1[String, String] =
+  (str: String) => {
+    var s: String = str
+    val g: Map[Char, Int] = { for ( c: Char <- s)
+      yield (c -> s.indexOf(c)) }.toMap
+    for ( (k,v) <- g )
+      s = s.replaceAll(k.toString, v.toString)
+    s
+  }
--- src/main/scala/word.scala
+++ src/main/scala/word.scala
@@ -3,17 +3,32 @@ import com.DouglasCodes.CryptogramScala.Dictionary
 import com.DouglasCodes.CryptogramScala.LetterPool
 
 class Word(val name: String, val dictionary: Dictionary ) extends UnitOfLanguage[String] {
+  val pattern: String = NumericPatternOfString(name)
   val uName: String = OrderedUniqueString(name)
-  val value = for (i: Char <- uName.toVector)
-    yield LetterPool.refer(i)
+  val value =
+    for (i: Char <- uName.toVector)
+      yield LetterPool.refer(i)
 
   protected var possibles: Set[String] =
-    dictionary.words(name.size).filter( (a) => a._2 == uName.size ).keySet
+    dictionary.words(name.size).filter( (a) =>
+      a._2._1 == uName.size &&
+      a._2._2 == pattern ).keySet
+
+  def update(): Unit =
+    possibles = possibles.filter( (testWord: String) => this.canBe(OrderedUniqueString(testWord) ) )
+
+  override def canBe( testWord: String ): Boolean = {
+    if ( testWord.size != this.uName.size )
+      return false
+
+    for ( i: Int <- (0 until testWord.size) )
+      if ( !(value(i).canBe(testWord(i) ) ) )
+        return false
+
+    return true
+  }
 
   override def toString: String =
-    name + "\n" +
-      { for (p <- value)
-          yield p
-      }.mkString("","","") +
-     possibles.mkString("{ ", " ", " }")
+    name + "\n" + { for (p <- value) yield p }.mkString("","","") + possibles.mkString("{ ", " ", " }")
+
 }
